b6d42c3d4017b2a38d936cf56131e62986347d4c
Nícolas Iensen <nicolas@engage.is>
2012-11-16 12:08:32 -0200

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParadaCarioca::Application.config.secret_token = 'e91125827905be4cbc30ba7cd5e207f2f2ea34815aabf5d94c43838e5d83f4feed28cea41c851c627132d29ad9ff42fe53d83096632c2635d468cc8212ea8ea5'
