31a4e72ddee3ad77061ba8b00272c8f806074d79
Luiz Fonseca <runeroniek@gmail.com>
2012-11-21 15:55:06 -0200

    Refactoring and fixes.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,3 +1,6 @@
 class ActivitiesController < InheritedResources::Base
-  before_filter only: [:index] { @top_events = Event.by_popularity.limit(3) }
+  before_filter only: [:index] do 
+    @recent_events = Event.by_most_recent.limit(5)
+    @top_events = Event.by_popularity.limit(3)
+  end
 end
