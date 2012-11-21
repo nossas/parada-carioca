776ebfa41a722f3869ec75483beefad205da4dbd
Luiz Fonseca <runeroniek@gmail.com>
2012-11-21 16:38:43 -0200

    Initial implementation of activities#show with no style.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ParadaCarioca::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
   
+  resources :activities, only: [:index, :show]
 
   get '/auth/facebook', as: :facebook_auth
   root :to => 'activities#index'
