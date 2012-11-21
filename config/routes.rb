bc4da2f58927b23bc59df26201d918b4e4820271
Luiz Fonseca <runeroniek@gmail.com>
2012-11-21 17:48:59 -0200

    Instead of activities, show events. Our main target are events, not activities (that have one or more events).

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ParadaCarioca::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
   
-  resources :activities, only: [:index, :show]
+  resources :activities, only: [:show] do 
+    resources :events, only: [:index, :show]
+  end
 
   get '/auth/facebook', as: :facebook_auth
-  root :to => 'activities#index'
+  root :to => 'events#index'
 end
