98de99fec6349a93ea6bdb5f180d55f0ebb8fa73
Luiz Fonseca <runeroniek@gmail.com>
2012-11-22 21:39:58 -0200

    Fix styles and remove duplicate CSS lines.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 
   before_filter only: [:index] do 
     @recent_events = Event.by_most_recent.limit(5)
-    @most_popular_guides = User.by_popularity.limit(5)
+    @popular_guides = User.by_popularity.limit(5)
     @top_events = Event.by_popularity.limit(3)
   end
 end
