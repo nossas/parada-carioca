9e2d5ac6a4713f83b3097588a561b7c817ed6d7e
Nícolas Iensen <nicolas@engage.is>
2012-11-22 20:14:47 -0200

    #527043 working test view_the_most_popular_guides.feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :avatar, :uid
   has_many :activities
   validates :email, :first_name, :presence => true
+  scope :by_popularity, order("(SELECT count(*) FROM participations JOIN events ON participations.event_id = events.id JOIN activities ON activities.id = events.activity_id WHERE activities.user_id = users.id) DESC")
 
   def name
     "#{self.first_name} #{self.last_name}"
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -13,9 +13,22 @@ Then /^the (\d+).* most recent event should be "(.*?)"$/ do |arg1, arg2|
   page.should have_css(".most_recent_events li.event:nth-child(#{arg1})", :text => truncate(arg2, :length => 25))
 end
 
+Given /^there is a guide called "(.*?)" and email "(.*?)"$/ do |arg1, arg2|
+  User.make! :first_name => arg1.split[0], :last_name => arg1.split[1], :email => arg2
+end
+
+Given /^there is an activity of "(.*?)" with (\d+) attendees$/ do |arg1, arg2|
+  event = Event.make! :activity => Activity.make!(:guide => User.find_by_email(arg1))
+  arg2.to_i.times { event.attendees << User.make! }
+end
+
+Then /^the (\d+).* most popular guide should be "(.*?)"$/ do |arg1, arg2|
+  page.should have_css(".most_popular_guides li.guide:nth-child(#{arg1})", :text => truncate(arg2, :length => 25))
+end
+
+Then(/^show me the page$/)                      { save_and_open_page }
 When(/^I go to "(.*?)"$/)                       { |arg1| visit path(arg1) }
 Then(/^the first activity should be "(.*?)"$/)  { |arg1| page.should have_css('ol.activities li:first-child', :text => arg1) }
-Then(/^show me the page$/)                      { save_and_open_page }
 Then(/^I should see "(.*?)"$/)                  { |arg1| page.should have_content(arg1) }
 When(/^I click "(.*?)"$/)                       { |arg1| click_link link(arg1) }
 Then(/^I should see "(.*?)" component$/)        { |arg1| page.should have_css(component(arg1)) }
