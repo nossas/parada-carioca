b97e0f4480a300946df34a065303dc537a5782cb
Nícolas Iensen <nicolas@engage.is>
2012-11-30 18:20:20 -0200

    #525742 Add description

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -3,7 +3,7 @@ require 'machinist/active_record'
 
 Activity.blueprint do
   name          { "Pular da Pedra da Gávea de asa-delta" }
-  description   { "Lorem ipsum..." }
+  description   { "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." }
   guide         { User.make! }
   price         { 100 }
   address       { "Estrada do Joá próximo ao número 450" }
@@ -19,6 +19,9 @@ User.blueprint do
 end
 
 Event.blueprint do
-  date      { Time.now }
-  activity  { Activity.make! }
+  date              { Time.now }
+  activity          { Activity.make! }
+  duration          { 5 }
+  minimum_attendees { 2 }
+  maximum_attendees { 10 }
 end
