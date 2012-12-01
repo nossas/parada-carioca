f93dd265f19ba135b6724fd3729ed1f60be45f5e
Nícolas Iensen <nicolas@engage.is>
2012-12-01 09:58:41 -0200

    #525742 add guide info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :avatar, :uid
+
   has_many :activities
+  has_many :reviews, :through => :activities
+
   validates :email, :first_name, :presence => true
   scope :by_popularity, order("(SELECT count(*) FROM participations JOIN events ON participations.event_id = events.id JOIN activities ON activities.id = events.activity_id WHERE activities.user_id = users.id) DESC")
 
