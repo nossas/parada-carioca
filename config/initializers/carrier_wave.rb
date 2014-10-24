7df3579e05fbb7f48b1d2708fb5f0044b7a749c0
Nícolas Iensen <nicolas@iensen.me>
2014-10-24 10:09:12 -0200

    Change Amazon bucket name

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV["AWS_ID"],
     :aws_secret_access_key  => ENV["AWS_SECRET"],
   }
-  config.fog_directory  = 'paradacarioca' 
+  config.fog_directory  = 'paradacarioca-production' 
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
